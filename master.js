03527b66454c73704ba4fa15fa82d23d89365b50
Mat <49793638+Gitchegumi@users.noreply.github.com>
2023-11-21 00:58:36 -0500

    Refactor code to use MutationObserver for updating active links

--- master.js
+++ master.js
@@ -1,11 +1,22 @@
-window.onload = function() {
-    var currentLocation = window.location.href;
-    $('#toplinks .nav-link').each(function() {
-        var linkHref = $(this).attr('href');
-        if (linkHref === currentLocation) {
-            $(this).addClass('active');
-        } else {
-            $(this).removeClass('active');
-        }
+$(document).ready(function() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList') {
+                var currentLocation = window.location.href;
+                $('#toplinks .nav-link').each(function() {
+                    var linkHref = $(this).attr('href');
+                    if (linkHref === currentLocation) {
+                        $(this).addClass('active');
+                    } else {
+                        $(this).removeClass('active');
+                    }
+                });
+            }
+        });
     });
-};
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+});
