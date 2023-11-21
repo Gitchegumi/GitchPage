14eafb62d8ca13a645bea3ae7774e66f1fa45f70
Mat <49793638+Gitchegumi@users.noreply.github.com>
2023-11-21 00:19:08 -0500

    update master.js

--- master.js
+++ master.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     var currentLocation = window.location.pathname;
-    $('header-component a').each(function() {
+    $('header a').each(function() {
         var linkPath = $(this).attr('href');
-        if ((linkPath === currentLocation) || (linkPath === 'index.html' && currentLocation === '/')) {
+        if ((linkPath === currentLocation) || (linkPath === '/index.html' && currentLocation === '/')) {
             $(this).addClass('active');
         } else {
             $(this).removeClass('active');
