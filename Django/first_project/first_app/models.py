6012f6b55af11a2274d6ab0ac1eba87b983f475f
Mat <49793638+Gitchegumi@users.noreply.github.com>
2023-12-26 21:51:45 -0500

    Remove __pycache__ stuff

--- Django/first_project/first_app/models.py
+++ Django/first_project/first_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    """Model representing a topic"""
+
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        """String representation of the Topic model"""
+        return self.top_name
+
+
+class Webpage(models.Model):
+    """Model representing a webpage"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=264, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        """String representation of the Webpage model"""
+        return self.name
+
+
+class AccessRecord(models.Model):
+    """Model representing an access record"""
+
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        """String representation of the AccessRecord model"""
+        return str(self.date)
