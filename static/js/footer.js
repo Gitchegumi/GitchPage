bf45de776d75628c984e3f6ea6d578ec3484aff3
Mat <49793638+Gitchegumi@users.noreply.github.com>
2024-08-15 09:53:09 -0400

    feat: adding footer to website (#1)

---
+++ static/js/footer.js
@@ -0,0 +1,40 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <footer class="footer mt-auto py-3 bg-light">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <h5>Site Map</h5>
+                <ul class="list-unstyled">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/portfolio.html">Portfolio</a></li>
+                  <li><a href="/voice_over.html">Voice Over</a></li>
+                  <li><a href="https://store.gitchegumi.com">Merch</a></li>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <h5>Social Media</h5>
+                <ul class="list-unstyled">
+                  <li><a href="https://www.facebook.com/GitchegumiGaming" target="_blank">Facebook</a></li>
+                  <li><a href="https://www.instagram.com/gitchegumi" target="_blank">Instagram</a></li>
+                  <li><a href="https://twitter.com/GitchegumiGames" target="_blank">Twitter</a></li>
+                  <li><a href="https://www.twitch.tv/gitchegumi" target="_blank">Twitch</a></li>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <p>&copy; 2024 GitcheGumi Media LLC. All rights reserved.</p>
+                <p>Designed and developed by Mathew Lindholm</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
