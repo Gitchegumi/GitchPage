37ccdbe9dc2cadceee020ec92ad3cd1912313321
gitchegumi <mat@gitchegumi.com>
2024-08-21 13:48:43 -0400

    initial commit for next.js

---
+++ gitchegumi-nextjs/app/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./globals.css";
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
