214ec45cd51e370511db3adbec49987b9ddf4f06
gitchegumi <mat@gitchegumi.com>
2024-09-07 11:10:02 -0400

    update to file structure and preparing for refactor

--- gitchegumi-nextjs/app/layout.tsx
+++ gitchegumi-nextjs/app/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
 import { ReactNode } from "react";
 
