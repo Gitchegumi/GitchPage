4f19926df5f8139ce0b59779d0386a9fa4939225
gitchegumi <mat@gitchegumi.com>
2024-09-10 10:50:25 -0400

    update to Voice Over demo layout

--- app/voice-over/page.tsx
+++ app/voice-over/page.tsx
@@ -13,37 +13,39 @@ export default function VoiceOver() {
   return (
     <main className="container mx-auto px-4 py-8">
       <div className="space-y-8">
-        <section>
-          <h2 className="text-2xl font-bold mb-4">Commercial Demo</h2>
-          <audio controls className="w-full mb-4">
-            <source src='/assets/demos/GITCHEGUMI-MEDIA_COMMERICAL-DEMO.mp3' type='audio/mpeg' />
-            Your browser does not support the audio element.
-          </audio>
-          <Button variant='default' className="z-40 mt-2 text-black bg-brand-blue-light hover:bg-white dark:text-white dark:bg-brand-blue-dark dark:hover:bg-brand-blue">
-            <a
-              href='/assets/demos/GITCHEGUMI-MEDIA_COMMERICAL-DEMO.mp3'
-              download
-            >
-              Download Commercial Demo
-            </a>
-          </Button>
-        </section>
+        <div className='flex flex-row justify-around'>
+          <section className='flex flex-col items-center'>
+            <h2 className="text-2xl font-bold mb-4">Commercial Demo</h2>
+            <audio controls className="w-[20rem] mb-4">
+              <source src='/assets/demos/GITCHEGUMI-MEDIA_COMMERICAL-DEMO.mp3' type='audio/mpeg' />
+              Your browser does not support the audio element.
+            </audio>
+            <Button variant='default' className="z-40 w-[15rem] mt-2 text-black bg-brand-blue-light hover:bg-white dark:text-white dark:bg-brand-blue-dark dark:hover:bg-brand-blue">
+              <a
+                href='/assets/demos/GITCHEGUMI-MEDIA_COMMERICAL-DEMO.mp3'
+                download
+              >
+                Download Commercial Demo
+              </a>
+            </Button>
+          </section>
 
-        <section>
-          <h2 className="text-2xl font-bold mb-4">E-learning Demo</h2>
-          <audio controls className="w-full mb-4">
-            <source src='/assets/demos/GITCHEGUMI-MEDIA_ELEARNING-DEMO.mp3' type='audio/mpeg' />
-            Your browser does not support the audio element.
-          </audio>
-          <Button variant='default' className="z-40 mt-2 text-black bg-brand-blue-light hover:bg-white dark:text-white dark:bg-brand-blue-dark dark:hover:bg-brand-blue">
-            <a
-              href='/assets/demos/GITCHEGUMI-MEDIA_ELEARNING-DEMO.mp3'
-              download={true}
-            >
-                Download E-learning Demo
-            </a>
-          </Button>
-        </section>
+          <section className='flex flex-col items-center'>
+            <h2 className="text-2xl font-bold mb-4">E-learning Demo</h2>
+            <audio controls className="w-[20rem] mb-4">
+              <source src='/assets/demos/GITCHEGUMI-MEDIA_ELEARNING-DEMO.mp3' type='audio/mpeg' />
+              Your browser does not support the audio element.
+            </audio>
+            <Button variant='default' className="z-40 w-[15rem] mt-2 text-black bg-brand-blue-light hover:bg-white dark:text-white dark:bg-brand-blue-dark dark:hover:bg-brand-blue">
+              <a
+                href='/assets/demos/GITCHEGUMI-MEDIA_ELEARNING-DEMO.mp3'
+                download={true}
+              >
+                  Download E-learning Demo
+              </a>
+            </Button>
+          </section>
+        </div>
 
         <section className="bg-gray-100 dark:bg-gray-800 p-8 rounded-lg shadow-md shadow-brand-blue-dark">
           <h2 className="text-3xl font-bold mb-4">Bring Your Project to Life with Professional Voiceover</h2>
