8343dbafb56852cdfb57ba6ca4217f3f89d01a4d
Gitchegumi <mat@gitchegumi.com>
2025-07-05 19:40:46 -0400

    added script for google analytics

--- next-site/src/app/layout.tsx
+++ next-site/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Oswald, Roboto_Serif } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -32,6 +33,19 @@ export default function RootLayout({
       <body
         className={`${oswald.variable} ${robotoSerif.variable} antialiased bg-brand-dark`}
       >
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TH478GZSDH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TH478GZSDH');
+          `}
+        </Script>
         <Header />
         <main>{children}</main>
         <Footer />
