dccd59f08e1a2ee08a4bbfc0754f89b75a7a5464
Gitchegumi <mat@gitchegumi.com>
2025-07-06 22:45:00 -0400

    changed badge positions on technical CV

--- next-site/src/components/utilities/Certifications.tsx
+++ next-site/src/components/utilities/Certifications.tsx
@@ -25,16 +25,18 @@ export default function Certifications() {
               data-share-badge-id="817446c4-03a9-4d42-9304-c3c8c68cb858"
               data-share-badge-host="https://www.credly.com"
             />
-            <div
-              data-iframe-width="150"
-              data-iframe-height="270"
-              data-share-badge-id="f204287b-b301-4cdf-bcd5-8c159c2a704f"
-              data-share-badge-host="https://www.credly.com"
-            />
+
             <div
               data-iframe-width="150"
               data-iframe-height="270"
               data-share-badge-id="9e57daba-0304-453a-a1b9-a6b6fb7437dc"
+              data-share-badge-host="https://www.credly.com"
+            />
+
+            <div
+              data-iframe-width="150"
+              data-iframe-height="270"
+              data-share-badge-id="f204287b-b301-4cdf-bcd5-8c159c2a704f"
               data-share-badge-host="https://www.credly.com"
             />
           </>
@@ -52,21 +54,26 @@ export default function Certifications() {
             src="//cdn.credly.com/assets/utilities/embed.js"
           />
         )}
-        <div className="flex flex-col items-center rounded shadow">
+        <div className="flex flex-col justify-center items-center rounded shadow">
           <Image
             src="/images/microsoft-certified-associate-badge.svg"
             alt="Microsoft Certified: Azure Administrator Associate"
-            width={150}
-            height={150}
+            width={100}
+            height={100}
           />
-          <p className="mt-2 text-xs font-medium text-brand-dark">
-            Microsoft Certified: Azure Administrator Associate (AZ-104)
-          </p>
+          <span className="px-2 text-center">
+            <p className="mt-2 text-xs font-medium text-brand-dark">
+              Microsoft Certified:
+            </p>
+            <p className="mt-1 text-xs font-medium text-brand-dark">
+              Azure Administrator Associate (AZ-104)
+            </p>
+          </span>
           <a
             href="https://learn.microsoft.com/en-us/users/MatL-9995/credentials/C2EFA69B2E4C57DC"
             target="_blank"
             rel="noopener noreferrer"
-            className="underline text-brand-blue"
+            className="text-xs underline text-brand-blue"
           >
             View certification
           </a>
