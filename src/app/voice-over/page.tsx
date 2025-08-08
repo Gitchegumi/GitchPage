aa9f369d77975ded5fa6cf3cb25c699460f176e4
Gitchegumi <mat@gitchegumi.com>
2025-08-07 21:59:55 -0400

    Updated the format and position of the form for the voice over page

--- src/app/voice-over/page.tsx
+++ src/app/voice-over/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { AudioPlayer } from "@/components/AudioPlayer";
 import { ContactForm } from "@/components/utilities/ContactForm";
 
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Gitchegumi Media | Voice Over Demos',
+  title: "Gitchegumi Media | Voice Over Demos",
 };
 
 export default async function VoiceOverPage() {
@@ -45,27 +45,33 @@ export default async function VoiceOverPage() {
           />
         </div>
       </div>
-      <div className="flex flex-row gap-8 justify-center items-start my-8">
-        <Image
-          src="/images/Background.png"
-          alt="Voice Over"
-          width={500}
-          height={500}
-          className="hidden rounded-lg shadow-lg md:block shadow-brand-blue/50"
-        />
-        <div className="w-full md:w-1/2">
-          <div className="mb-8">
-            <h2 className="mb-6 text-3xl font-bold text-center">
-              Why Choose Me?
-            </h2>
-            <ul className="mx-auto space-y-2 text-lg list-disc list-inside">
-              <li>Versatile voice suitable for various projects.</li>
-              <li>Quick turnaround times.</li>
-              <li>Professional home studio for consistent quality.</li>
-              <li>Collaborative approach to ensure your vision is realized.</li>
-            </ul>
+      <div className="flex flex-col items-center my-8">
+        <div className="flex flex-row gap-8 justify-center items-start my-8">
+          <Image
+            src="/images/Background.png"
+            alt="Voice Over"
+            width={500}
+            height={500}
+            className="hidden rounded-lg shadow-lg md:block shadow-brand-blue/50"
+          />
+          <div className="w-full md:w-1/2">
+            <div className="mb-8">
+              <h2 className="mb-6 text-3xl font-bold text-center">
+                Why Choose Me?
+              </h2>
+              <ul className="mx-auto space-y-2 text-lg list-disc list-inside">
+                <li>Versatile voice suitable for various projects.</li>
+                <li>Quick turnaround times.</li>
+                <li>Professional home studio for consistent quality.</li>
+                <li>
+                  Collaborative approach to ensure your vision is realized.
+                </li>
+              </ul>
+            </div>
           </div>
-          <h2 className="mb-8 text-3xl font-bold text-center">Get in Touch</h2>
+        </div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-3xl font-bold text-center">Get in Touch</h2>
           <ContactForm />
         </div>
       </div>
