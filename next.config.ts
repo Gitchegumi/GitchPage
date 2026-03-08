dab9d43eb9752321d606bb73225516cf5ddf01af
Gitchegumi <mat@gitchegumi.com>
2026-03-08 16:08:20 -0500

    chore: standardize string literals to double quotes and remove the `outputFileTracingRoot` configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
-import createMDX from '@next/mdx';
-import type { NextConfig } from 'next';
+import createMDX from "@next/mdx";
+import type { NextConfig } from "next";
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
 });
 
 const nextConfig: NextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  output: 'export',
+  basePath: process.env.NODE_ENV === "production" ? "" : "",
+  output: "export",
   images: {
     unoptimized: true,
   },
-  pageExtensions: ['ts', 'tsx', 'mdx', 'md', 'js', 'jsx'],
-  outputFileTracingRoot: '../..',
+  pageExtensions: ["ts", "tsx", "mdx", "md", "js", "jsx"],
 };
 
 export default withMDX(nextConfig);
